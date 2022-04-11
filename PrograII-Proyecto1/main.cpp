1f34b22164d90cd0fc1b85865c68c0b0ef7864f4
ALEXANDER DITTEL ESCOBAR <alexander.dittel.escobar@est.una.ac.cr>
2022-04-11 17:26:57 -0600

    v1.0 comments

--- PrograII-Proyecto1/main.cpp
+++ PrograII-Proyecto1/main.cpp
@@ -1,7 +1,14 @@
+//=================================================//
+// Prueba de github
+// 
+// 
+//=================================================//
+
 #include <iostream>
 using namespace std;
 
 int main() {
 	cout << "hola" << endl;
+	cout << "como esta :v" << endl;
 	return 0;
 }
